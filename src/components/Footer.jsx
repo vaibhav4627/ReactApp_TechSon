8ea3bcb05b335baf3d152d387456425de4a6063e
vaibhav4627 <vaibhavbajpai4627@gmail.com>
2023-06-06 20:54:32 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.linkedin.com/in/vaibhav-bajpai-42841b22a/" target={"blank"}>
+            Linkedin
+          </a>
+          <a href="https://github.com/vaibhav4627" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
